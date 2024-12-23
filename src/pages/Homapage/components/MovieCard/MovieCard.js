e41bdb09dcbc8fcae8c6aeb7dd460e49d0ac41d4
신윤호 <xode114kr1@naver.com>
2024-12-24 01:59:02 +0900

    feat : MovieCard 제작

--- src/pages/Homapage/components/MovieCard/MovieCard.js
+++ src/pages/Homapage/components/MovieCard/MovieCard.js
@@ -1,7 +1,31 @@
 import React from "react";
+import "./MovieCard.style.css";
+import { Badge } from "react-bootstrap";
 
 const MovieCard = ({ movie, index }) => {
-  return <div>Movic</div>;
+  return (
+    <div
+      style={{
+        backgroundImage:
+          "url(https://media.themoviedb.org/t/p/w300_and_h450_bestv2" +
+          `${movie.poster_path}` +
+          ")",
+      }}
+      className="movie-card"
+    >
+      <div className="overlay">
+        <h3>{movie.title}</h3>
+        {movie.genre_ids.map((id) => (
+          <Badge bg="danger">{id}</Badge>
+        ))}
+        <div>
+          <div>{movie.vote_average}</div>
+          <div>{movie.popularity}</div>
+          <div>{movie.adult ? "over18" : "under18"}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieCard;
--- src/pages/Homapage/components/PopularMovieSlide/PopularMovieSlide.js
+++ src/pages/Homapage/components/PopularMovieSlide/PopularMovieSlide.js
@@ -12,7 +12,7 @@ const PopularMovieSlide = () => {
   const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 8,
+      items: 6,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
